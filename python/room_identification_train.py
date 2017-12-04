9b84662afc4cbdf75bf31d6a1aec901c78474f7a
Chris Raff <crharfifs@gmail.com>
2017-12-04 16:20:32 -0500

    Added ability to graph mean freq spectrum for rooms :chart_with_upwards_trend:

--- python/room_identification_train.py
+++ python/room_identification_train.py
@@ -47,8 +47,8 @@ if not os.path.exists(output_dir):
 # the filenames should be in the form 'room-data-subject-1.csv', e.g. 'room-data-Erik-1.csv'. If they
 # are not, that's OK but the progress output will look nonsensical
 
-class_names = 'eng_lab_304 eng_lab_hallway_box eng_lab_307B'.split()
-# class_names = 'chris_bedroom downstairs_bathroom kitchen living_room staircase alex_bedroom upstairs_bathroom'.split()
+# class_names = 'eng_lab_304 eng_lab_hallway_box eng_lab_307B'.split()
+class_names = 'chris_bedroom downstairs_bathroom kitchen living_room staircase alex_bedroom upstairs_bathroom'.split()
 
 data = np.zeros((0, 8002))  # 8002 = 1 (timestamp) + 8000 (for 8kHz audio data) + 1 (label)
 
@@ -89,17 +89,39 @@ sys.stdout.flush()
 X = np.zeros((0, n_features))
 y = np.zeros(0,)
 
-
+graph = True
+if graph:
+    mean_freqs = [None] * len(class_names)
 
 for i, window_with_timestamp_and_label in enumerate(data):
     window = window_with_timestamp_and_label[1:-1]
     label = data[i, -1]
+
+    if graph:
+        if mean_freqs[int(label)] is None:
+            mean_freqs[int(label)] = np.fft.fft(window)[1:]
+        else:
+            mean_freqs[int(label)] += np.fft.fft(window)[1:]
+
     # print("Extracting features for window " + str(i) + "...")
     x = feature_extractor.extract_features(window)
     if (len(x) != X.shape[1]):
         print("Received feature vector of length {}. Expected feature vector of length {}.".format(len(x), X.shape[1]))
     X = np.append(X, np.reshape(x, (1, -1)), axis=0)
     y = np.append(y, label)
+
+if graph:
+    import matplotlib.pyplot as plt
+
+    for i, mf in enumerate(mean_freqs):
+        if mf is not None:
+            mf /= len(data)
+
+            plt.plot(mf)
+            plt.title("mean freqencies for " + class_names[i])
+            plt.show()
+
+
 
 
 # print("oversampling the data")
